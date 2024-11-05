d54eb2326c5030c7f2f20cf5b05acaf80942f744
Oleksandr <oleks.bodnar@gmail.com>
2024-11-05 21:45:32 +0200

    add redirect for other routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
@@ -26,18 +26,23 @@ export const App = () => {
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
         <Route
           path="/register"
           element={<RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />}
         />
+
         <Route
           path="/login"
           element={<RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />}
         />
+
         <Route
           path="/contacts"
           element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />}
         />
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Layout>
   );
